f54dee518d159add994876f162dfb08ab9a48281
senntyou <jiangjinbelief@163.com>
2018-10-25 10:16:55 +0800

    almost finish clean cmd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+import fs from 'fs';
 import commander from 'commander';
 import inquirer from 'inquirer';
+import ora from 'ora';
+import del from 'del';
+import readdir from 'recursive-readdir';
 
 import pkg from '../package.json';
 import { registerArchiveTask, registerRestoreTask } from './tasks';
-import { getArchiveName, getZipFile } from './util';
+import { getArchiveName, getZipFile, hashCodes, filesToDelete } from './util';
 import { log } from './logger';
 import run from './run';
 
+const { existsSync } = fs;
 const { prompt } = inquirer;
 
 // version
@@ -30,9 +35,9 @@ commander
 commander
   .command('restore <directory>')
   .description('restore target directory to last nth archive state')
-  .option('-i, --index [index]', 'last nth state to restore')
-  .action((dir, { index = 1 }) => {
-    const file = getZipFile(dir, index);
+  .option('-i, --index [index]', 'last nth state to restore', 1)
+  .action((dir, { index }) => {
+    const file = getZipFile(dir, parseInt(index, 10));
 
     if (!file) {
       log(`
@@ -54,7 +59,66 @@ commander
       run('restore', () => {
         log(`
   restored ${file} to ${dir}    
-      `);
+        `);
+      });
+    });
+  });
+
+// clean
+commander
+  .command('clean <directory>', 'clean obsolete hash-code files')
+  .option('-e, --ext [ext]', 'html extension', 'html')
+  .option('-H, --hash [hash]', 'hash length', 32)
+  .action((dir, { ext, hash }) => {
+    if (!existsSync(dir)) return;
+
+    const spinner = ora('loading files').start();
+    readdir(dir, (err, files) => {
+      if (err) throw err;
+
+      if (!files || !files.length) {
+        spinner.stop();
+        return;
+      }
+
+      const result = hashCodes({ files, ext, hash });
+
+      if (result.error) {
+        log(`
+  ${result.message}    
+        `);
+        return;
+      }
+
+      const { codes } = result;
+      const delFiles = filesToDelete({ files, hash, codes });
+
+      spinner.stop();
+      prompt([
+        {
+          name: 'goon',
+          type: 'confirm',
+          message: `${
+            delFiles.length
+          } files to be deleted, are you sure to continue?`,
+        },
+      ]).then(({ goon }) => {
+        if (!goon) return;
+
+        const archiveName = getArchiveName(dir);
+        registerArchiveTask(dir, archiveName);
+        run('archive', () => {
+          spinner.text = 'deleting files';
+          spinner.start();
+
+          del(delFiles).then(() => {
+            spinner.stop();
+
+            log(`
+  cleaned ${delFiles.length} files
+            `);
+          });
+        });
       });
     });
   });
--- src/util.js
+++ src/util.js
@@ -1,6 +1,17 @@
+import fs from 'fs';
 import glob from 'glob';
 
-export const fillTwoChars = num => (num > 9 ? `${num}` : `0${num}`);
+const { readFileSync } = fs;
+
+const fillTwoChars = num => (num > 9 ? `${num}` : `0${num}`);
+
+const correctSlash = str => str.replace(/(\\\\|\\)/g, '/');
+
+const getFileName = file => {
+  const correctFile = correctSlash(file);
+  const lastIndex = correctFile.lastIndexOf('/');
+  return lastIndex === -1 ? correctFile : correctFile.slice(lastIndex + 1);
+};
 
 export const getArchiveName = dir => {
   const now = new Date();
@@ -22,4 +33,84 @@ export const getZipFile = (dir, index = 1) => {
   if (!files || !files.length) return null;
 
   return files[files.length - index];
+};
+
+const makeRegExp = hash =>
+  new RegExp(`[^0-9a-f]([0-9a-f]{${hash}})[^0-9a-f]`, 'g');
+
+export const hashCodes = ({ files, ext, hash }) => {
+  const htmlCodes = [];
+  const jsCodes = [];
+
+  let htmlCount = 0;
+
+  // hash codes in html content
+  files.forEach(file => {
+    if (file.slice(0 - ext.length) !== ext) return;
+
+    const content = readFileSync(file);
+    const regExp = makeRegExp(hash);
+
+    let result = regExp.exec(content);
+    while (result) {
+      htmlCodes.push(result[1]);
+      result = regExp.exec(content);
+    }
+
+    htmlCount += 1;
+  });
+
+  if (!htmlCount)
+    return {
+      error: !0,
+      message: `no ${ext} files found`,
+    };
+
+  // hash codes in js content
+  files.forEach(file => {
+    if (file.slice(-3) !== '.js') return;
+
+    const fileRegExp = makeRegExp(hash);
+    const fileResult = fileRegExp.exec(`/${getFileName(file)}`);
+
+    if (!fileResult) return;
+
+    const fileHashCode = fileResult[1]; // eslint-disable-line prefer-destructuring
+
+    if (htmlCodes.indexOf(fileHashCode) < 0) return;
+
+    const content = readFileSync(file);
+    const regExp = makeRegExp(hash);
+
+    let result = regExp.exec(content);
+    while (result) {
+      jsCodes.push(result[1]);
+      result = regExp.exec(content);
+    }
+  });
+
+  return {
+    error: !1,
+    codes: [...htmlCodes, ...jsCodes],
+  };
+};
+
+export const filesToDelete = ({ files, hash, codes }) => {
+  const res = [];
+
+  files.forEach(file => {
+    // only clean js + css files
+    if (file.slice(-3) !== '.js' && file.slice(-4) !== '.css') return;
+
+    const fileRegExp = makeRegExp(hash);
+    const fileResult = fileRegExp.exec(`/${getFileName(file)}`);
+
+    if (!fileResult) return;
+
+    const fileHashCode = fileResult[1]; // eslint-disable-line prefer-destructuring
+
+    if (codes.indexOf(fileHashCode) < 0) res.push(file);
+  });
+
+  return res;
 };
