350a7371773d5e71fbcfcbd6777ffc03813cf66b
senntyou <jiangjinbelief@163.com>
2018-10-26 10:29:31 +0800

    remove old

--- old/test/clean/demo-2/bak/parent.456789aa.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-module.exports = { '19': '721ea8e8.async' };
--- old/test/restore/3.js
+++
@@ -1,3 +0,0 @@
-require('../../util/change_cwd')(__dirname + '/demo');
-
-require('../../util/exec')('sclean restore -i 3');
